952ad1593ace6a17c2c1b434798b7970908ee6b9
chokopops <44207965+chokopops@users.noreply.github.com>
2020-11-03 14:58:06 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,7 +6,7 @@ node {
     def buildInfo
     stage('Artifactory configuration') {
         // Tool name from Jenkins configuration
-        rtMaven.tool = "Maven"
+        rtMaven.tool = "maven"
         // Set Artifactory repositories for dependencies resolution and artifacts deployment.
         rtMaven.deployer releaseRepo:'libs-release-local', snapshotRepo:'libs-snapshot-local', server: server
         rtMaven.resolver releaseRepo:'libs-release', snapshotRepo:'libs-snapshot', server: server
